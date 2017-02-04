7bb8a34f7dd7013480b290f7bfc22b9d99d971a3
AsimHabib <asimhabib2@gmail.com>
2017-02-04 15:08:30 -0500

    Added 404 view and updated index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Portfolio' });
 });
 
+/* GET 404 page */
+router.get('/*', function(req, res, next) {
+  res.render('404', {title: '404'});
+});
+
 module.exports = router;
